67c7b64ce1fed7abe18bdc2ff3bdaaae60cfb4a6
dvlprkoji <koji4321@naver.com>
2021-07-29 17:32:59 +0900

    - 0729 Dev Merge

--- src/main/java/sptek/spdevteam/intern/content/service/impl/ListServiceImpl.java
+++ src/main/java/sptek/spdevteam/intern/content/service/impl/ListServiceImpl.java
@@ -1,17 +1,38 @@
 package sptek.spdevteam.intern.content.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sptek.spdevteam.intern.common.LogUtil;
+import sptek.spdevteam.intern.common.mybatis.CommonMapper;
+import sptek.spdevteam.intern.content.domain.ContentExcel;
 import sptek.spdevteam.intern.content.domain.ListDomain;
+import sptek.spdevteam.intern.content.domain.SrcDto;
+import sptek.spdevteam.intern.content.domain.TempType;
+import sptek.spdevteam.intern.content.mybatis.FilterMapper;
+import sptek.spdevteam.intern.content.mybatis.ListMapper;
 import sptek.spdevteam.intern.content.service.ListService;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
+@RequiredArgsConstructor
 public class ListServiceImpl implements ListService {
 
     private LogUtil log = new LogUtil(ListServiceImpl.class);
 
+    private final FilterMapper filterMapper;
+    private final ListMapper listMapper;
+
     @Override
-    public ListDomain listMethod(ListDomain listDomain) throws Exception {
-        return null;
+    public List<TempType> getTempList() {
+        return filterMapper.getTempList();
     }
+
+    @Override
+    public List<ContentExcel> getList(ContentExcel contentExcel) throws Exception {
+        return listMapper.getList(contentExcel);
+    }
+
+
 }
